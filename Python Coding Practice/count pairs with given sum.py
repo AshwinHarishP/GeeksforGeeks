7ad7146b92e54cbfaed63ab427db5961cd4a88a7
Ashwin Harish.P <99186533+AshwinHarishP@users.noreply.github.com>
2023-08-03 20:54:45 +0530

    Create count pairs with given sum.py

---
+++ Python Coding Practice/count pairs with given sum.py
@@ -0,0 +1,34 @@
+#User function Template for python3
+
+class Solution:
+    def getPairsCount(self, arr, n, k):
+        dic = dict()
+        count = 0
+        
+        for i in arr:
+            target = k - i
+            
+            if target in dic:
+                count += dic[target]
+                
+            dic[i] = dic.get(i, 0) + 1
+            
+        return count
+            
+                
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        n, k = list(map(int, input().strip().split()))
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        ans = ob.getPairsCount(arr, n, k)
+        print(ans)
+        tc -= 1
+
+# } Driver Code Ends
