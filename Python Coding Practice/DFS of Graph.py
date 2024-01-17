0cfeaa6bb59b3e900e4246bd9740e12feb974cc0
Ashwin Harish.P <99186533+AshwinHarishP@users.noreply.github.com>
2024-01-17 19:45:14 +0530

    Create DFS of Graph.py

---
+++ Python Coding Practice/DFS of Graph.py
@@ -0,0 +1,43 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to return a list containing the DFS traversal of the graph.
+    def dfsOfGraph(self, V, adj):
+        def dfs(node):
+            visited[node] = True
+            res.append(node)
+            
+            for i in adj[node]:
+                if not visited[i]:
+                    dfs(i)
+                    
+                
+        
+        visited = [False] * V
+        res = []
+        for i in range(V):
+            if not visited[i]:
+                dfs(i)
+        return res
+
+
+#{ 
+ # Driver Code Starts
+
+if __name__ == '__main__':
+    T=int(input())
+    while T>0:
+        V,E=map(int,input().split())
+        adj=[[] for i in range(V+1)]
+        for i in range(E):
+            u,v=map(int,input().split())
+            adj[u].append(v)
+            adj[v].append(u)
+        ob=Solution()
+        ans=ob.dfsOfGraph(V,adj)
+        for i in range(len(ans)):
+            print(ans[i],end=" ")
+        print()
+        T-=1
+# } Driver Code Ends
